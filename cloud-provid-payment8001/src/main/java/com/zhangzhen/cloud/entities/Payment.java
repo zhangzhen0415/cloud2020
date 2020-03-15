06faec9b68b3ec0fc302df4f8f69f647fff1d2c6
zhangzhen <zhen.zhang@trinitesolutions.com>
2020-03-15 15:57:23 +0800

    test

---
+++ cloud-provid-payment8001/src/main/java/com/zhangzhen/cloud/entities/Payment.java
@@ -0,0 +1,16 @@
+package com.zhangzhen.cloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * zz
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    private Long id;
+    private String serial;
+}
