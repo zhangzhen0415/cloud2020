6ee8589d137d63795006669178665738880cec56
zhangzhen <zhen.zhang@trinitesolutions.com>
2020-04-29 21:15:07 +0800

    eureka 集群配置

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.ribbon.RibbonClient;
 //import org.springframework.cloud.netflix.ribbon.RibbonClients;
@@ -12,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2020-02-18 17:20
  */
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration=MySelfRule.class)
 public class OrderMain80
 {
